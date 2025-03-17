c22096e0c358e202adc259e51716d7a7b7cf1490
Dariya Nazarova <myEmail@test.ru>
2025-03-17 20:48:33 +0300

    Redirect

---
+++ src/test/java/HomeworkRedirect.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HomeworkRedirect {
+    @Test
+    public void testRedirect(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
